320b14c5b4ffd867631e550b306a93637b51ac07
Vinicius Cano <viniciuscano.vc@gmail.com>
2018-09-05 15:14:47 -0300

    Implementando lógica para escrever texto no documento txt

--- ReadWriteTXT/ViewController.swift
+++ ReadWriteTXT/ViewController.swift
@@ -13,19 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //txt URL
         let path = Bundle.main.path(forResource: "Text", ofType: "txt")
         print("Path =", path)
-        
+
         let url = URL(fileURLWithPath: path!)
         print("URL =", url)
         
-        let contentString = try! String(contentsOf: url, encoding: .utf8)
+        //Read
+        var contentString = try! String(contentsOf: url, encoding: .utf8)
+
+        //Write
+        let text = contentString + "Escreva aqui o texto que deseja escrever no documento"
+        
+        do {
+            try text.write(to: url, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Erro", error)
+        }
+        
+        //Read the entire document
+        contentString = try! String(contentsOf: url, encoding: .utf8)
         print(contentString)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 }
 
